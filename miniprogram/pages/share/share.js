75e51cf4e7f99f36280b0c550974ab259a486bcd
王玥 <moonwang1999@126.com>
2020-06-15 20:48:08 +0800

    分享界面添加数据库（test）

--- miniprogram/pages/share/share.js
+++ miniprogram/pages/share/share.js
@@ -40,5 +40,23 @@ Page({
         activities: activities
       })
     })
+  },
+  addData:function(){
+    db.collection('Activities').add({
+      // data 字段表示需新增的 JSON 数据
+      data: {
+        //_id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
+        A_Schema: this.data.activities.A_Schema,
+        A_Location: this.data.activities.A_Location,
+        A_StartDate: this.data.activities.A_StartDate,
+        A_StartTime: this.data.activities.A_StartTime,
+        A_Create: this.data.activities.A_Create,
+        A_Participate: app.globalData.openid
+      },
+      success: res => {
+        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+        console.log(this.data.activities)
+      }
+    })
   }
 })
